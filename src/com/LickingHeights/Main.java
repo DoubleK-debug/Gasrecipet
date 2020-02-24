455f039f2d5a5b013a78c74913dd8d6ecf836205
Kaniel <Kaniel.kusi.2022@student.lhschools.org>
2020-02-24 12:30:03 -0500

    Inetllij commit

---
+++ src/com/LickingHeights/Main.java
@@ -0,0 +1,57 @@
+package com.LickingHeights;
+
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("+-----------------------------+");
+        System.out.println("|          Circle K           |");
+        System.out.println("|            Brand            |");
+        System.out.println("|   8861 E Broad St.          |");
+        System.out.println("|   Reynoldsburg,OH 43062     |");
+        System.out.println("|                             |");
+        System.out.println("|Date                     Time|");
+        System.out.println("|13JAN2020               12:31|");
+        System.out.println("|Invoice #             694201 |");
+        System.out.println("|Auth #                 914264|");
+        System.out.println("| MasterCard  Account Number  |");
+        System.out.println("|       **********539710      |");
+        System.out.println("|Pump     Product         $/G |");
+        System.out.println("| 6          UNLD       $2.746|");
+        System.out.println("| Fuel Total            $43.94|");
+        System.out.println("| Credit                $43.94|");
+        System.out.println("|         THANK YOU !         |");
+        System.out.println("|          Circle K           |");
+        System.out.println("+-----------------------------+");
+
+
+
+
+
+
+
+
+     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
